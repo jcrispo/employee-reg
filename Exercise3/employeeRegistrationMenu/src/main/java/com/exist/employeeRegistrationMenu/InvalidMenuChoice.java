9c0c40f0cef0f976ed29c286aa7caa4405c54bae
jcrispo <jason.crispo@gmail.com>
2014-08-04 15:59:15 +0000

    Removed unused imports in InvalidMenuChoice.java

--- Exercise3/employeeRegistrationMenu/src/main/java/com/exist/employeeRegistrationMenu/InvalidMenuChoice.java
+++ Exercise3/employeeRegistrationMenu/src/main/java/com/exist/employeeRegistrationMenu/InvalidMenuChoice.java
@@ -1,8 +1,6 @@
 package com.exist.employeeRegistrationMenu;
 
-import java.util.HashMap;
 import java.util.Map;
-import java.util.ArrayList;
 import java.util.List;
 import com.exist.employeeInfoPackage.EmployeeData;
 
