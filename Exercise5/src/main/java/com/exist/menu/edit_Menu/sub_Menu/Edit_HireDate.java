a99bb1b9b8cc77de5dcbe363677d36ac21fc3061
jcrispo <jason.crispo@gmail.com>
2014-08-15 15:01:43 +0000

    Wrote code in Edit_HireDate.java. Editing Hire Date now works.

--- Exercise5/src/main/java/com/exist/menu/edit_Menu/sub_Menu/Edit_HireDate.java
+++ Exercise5/src/main/java/com/exist/menu/edit_Menu/sub_Menu/Edit_HireDate.java
@@ -5,9 +5,25 @@ import com.exist.main_Functions.EmployeeDataValidation;
 import com.exist.main_Functions.Database;
 
 public class Edit_HireDate extends EditMenu {
+    private static EmployeeDataValidation validate;
+    private static Database companyDatabase;
+    private static Scanner input;
+    private static String userInput;
+    private static final String updateSql = "UPDATE companyEmployeeData SET hireDate = \'";
+    private static final String conditionSql = "\' WHERE companyEmployeeData.employeeId = ";
+
+    public Edit_HireDate () {
+        companyDatabase = new Database();
+        input = new Scanner(System.in);
+        validate = new EmployeeDataValidation();
+    }
 
     public void execute (String userInput, String employeeNumberInput) {
-
+        userInput = validate.dateString("Hire Date: ");
+        companyDatabase.loginDatabase();
+        companyDatabase.insertStatement(updateSql + userInput + conditionSql + employeeNumberInput);
+        companyDatabase.updateDatabase();
+        companyDatabase.closeDatabase();
     }
 
     public boolean exitEditMenu () {
