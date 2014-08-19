42621c2105f1b0544a5f7a6d06dff31cc4bc0364
jcrispo <jason.crispo@gmail.com>
2014-08-19 19:01:20 +0000

    Added PositionData class to temporarily hold position data

---
+++ Exercise5/src/main/java/com/exist/main_Functions/PositionData.java
@@ -0,0 +1,23 @@
+package com.exist.main_Functions;
+
+public class PositionData {
+    private String position;
+    private Integer deptId;
+
+    public void addPositionName (String newPosition) {
+        position = newPosition;
+    }
+
+    public void setDeptId (Integer id) {
+        deptId = id;
+    }
+
+    public String getPositionName () {
+        return position;
+    }
+
+    public Integer getDepartmentId () {
+        return deptId;
+    }
+
+}
