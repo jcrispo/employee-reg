0ef5855c8f9ecf15504551aca393050b4d84c754
jcrispo <jason.crispo@gmail.com>
2014-08-22 18:29:53 +0000

    Database connection uses property file to get url, user, and password. Removed the use of Database class.

--- Exercise5/src/main/java/com/exist/main/EmployeeRegistration.java
+++ Exercise5/src/main/java/com/exist/main/EmployeeRegistration.java
@@ -4,6 +4,11 @@ import java.sql.DriverManager;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Scanner;
+import java.util.Properties;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.IOException;
+import java.io.FileNotFoundException;
 import com.exist.main_Functions.Database;
 import com.exist.main_Functions.Insert;
 import com.exist.main_Functions.ViewEmployeeData;
@@ -12,24 +17,20 @@ import com.exist.menu.main_Menu.EmployeeRegistrationMenu;
 import com.exist.menu.main_Menu.MenuOptions;
 
 public class EmployeeRegistration {
+    private static Connection dbConnection;
     private static String userChoice;
     private static Scanner userInput;
-    private static Database companyDatabase;
     private static Insert insertToDatabase;
     private static ViewEmployeeData view;
     private static DatabaseRetrieve retrieveFromDatabase;
     private static EmployeeRegistrationMenu menu;
     private static boolean exitMenu;
     private static final String BLANK = "b";
-    private static final String user = "root";
-    private static final String pass = "ex1stgl0bal";
-    private static final String dbUrl = "jdbc:mysql://localhost:3306/";
-    private static final String dbName = "Employee_Registration";
     private static final String menuOptions = "\nRegister 'r' | View 'v' | Edit 'e' | Delete 'd' | Exit 'x' : ";
 
     public EmployeeRegistration () {
+        dbConnection = null;
         userInput = new Scanner(System.in);
-        companyDatabase = new Database();
         insertToDatabase = new Insert();
         view = new ViewEmployeeData();
         retrieveFromDatabase = new DatabaseRetrieve();
@@ -58,18 +59,24 @@ public class EmployeeRegistration {
     }
 
     public void loginDatabase () {
-            boolean exitLogin = false;
-            companyDatabase.setCredentials(dbUrl + dbName, user, pass);
-            insertToDatabase.setCredentials(dbUrl + dbName, user, pass);
-            retrieveFromDatabase.setCredentials(dbUrl + dbName, user, pass);
-            view.setCredentials(dbUrl + dbName, user, pass);
-            exitLogin = companyDatabase.loginDatabase();
-            if (exitLogin) {
-                System.out.println("Login to Database Successful\n");
-            } else {
-                System.out.println("Wrong Username / Password! ");
-                System.exit(0);
-            }
-            companyDatabase.closeDatabase();
+        Properties dbProperty = new Properties();
+        try {
+            InputStream is = new FileInputStream("db.properties");
+            dbProperty.load(is);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            dbConnection = DriverManager.getConnection(dbProperty.getProperty("db.url"), dbProperty.getProperty("db.user"), dbProperty.getProperty("db.password"));
+            System.out.println("Login to Database Successful\n");
+        } catch (SQLException e) {
+            System.out.println("Wrong Username / Password! ");
+            System.exit(0);
+        }
+        insertToDatabase.setCredentials(dbProperty.getProperty("db.url"), dbProperty.getProperty("db.user"), dbProperty.getProperty("db.password"));
+        retrieveFromDatabase.setCredentials(dbProperty.getProperty("db.url"), dbProperty.getProperty("db.user"), dbProperty.getProperty("db.password"));
+        view.setCredentials(dbProperty.getProperty("db.url"), dbProperty.getProperty("db.user"), dbProperty.getProperty("db.password"));
     }
 }
