8196f0c5004c20784e91c747271063259011fa17
jcrispo <jason.crispo@gmail.com>
2014-09-02 09:55:03 +0000

    Modified: DBInsert class. Modified query statement for inserting email. Changes to catch MySQLIntegrityConstraintViolationException.

--- Exercise5/src/main/java/com/exist/database/DBInsert.java
+++ Exercise5/src/main/java/com/exist/database/DBInsert.java
@@ -6,12 +6,14 @@ import java.sql.PreparedStatement;
 import com.exist.mainFunctions.EmployeeData;
 import com.exist.mainFunctions.CompanyData;
 import com.exist.database.DBDataSource;
+import com.mysql.jdbc.exceptions.jdbc4.MySQLIntegrityConstraintViolationException;
 
 public class DBInsert {
     private static final String sqlStatement1 = "INSERT INTO personalInfo (firstName, middleName, lastName, gender, birthDate) VALUES (?, ?, ?, ?, ?)";
-    private static final String sqlStatement2 = "INSERT INTO companyEmployeeData (position_refId, hireDate, basicSalary, emailId) VALUES (?, ?, ?, ?)";
+    private static final String sqlStatement2 = "INSERT INTO companyEmployeeData (position_refId, hireDate, basicSalary) values (?, ?, ?)";
     private static final String sqlStatement3 = "INSERT INTO employeePosition (position_name, deptId) VALUES (?, ?)";
     private static final String sqlStatement4 = "INSERT INTO departments (dept_name) VALUES (?)";
+    private static final String sqlStatement5 = "UPDATE companyEmployeeData company JOIN personalInfo employee ON company.employeeId=employee.employeeId  set company.emailId = ? WHERE employee.firstName = ? AND employee.middleName = ? AND employee.lastName = ? AND employee.birthDate = ? ";
 
     public void editData (String sql) {
         Connection dbConnection = null;
@@ -60,10 +62,23 @@ public class DBInsert {
             insertStatement.setString(1, data.getPosition().toString());
             insertStatement.setString(2, data.getHireDate());
             insertStatement.setString(3, data.getSalary().toString());
-            insertStatement.setString(4, data.getEmail());
             insertStatement.executeUpdate();
+
+            insertStatement = dbConnection.prepareStatement(sqlStatement5);
+            insertStatement.setString(1, data.getEmail());
+            insertStatement.setString(2, data.getFirstName());
+            insertStatement.setString(3, data.getMiddleName());
+            insertStatement.setString(4, data.getLastName());
+            insertStatement.setString(5, data.getBirthDate());
+            insertStatement.executeUpdate();
+
         } catch (SQLException e) {
-            e.printStackTrace();
+            if (e instanceof MySQLIntegrityConstraintViolationException) {
+                System.out.println("Duplicate Entry! Adding given data to database Failed.");
+                e.printStackTrace();
+            } else {
+                e.printStackTrace();
+            }
         } finally {
             try {
                 if (insertStatement != null) {
